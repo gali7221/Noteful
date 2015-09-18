20b92bbae2e0b0b7c055b41daec817ecbc12d024
gali7221 <gali7221@gmail.com>
2015-09-18 01:30:12 +0000

    initial commit

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,9 +1,16 @@
 class Note < ActiveRecord::Base
+  
   belongs_to :user
 
   validates :title, presence: true
   validates :content, presence: true
 
   scope :sorted, lambda { order('created_at DESC') }
+  
+  
+  def preview
+    self.content #property of note stored in db. returning the content of the note instance
+    self.content.slice(0...100).concat "...(readmore)"
+  end
 
 end
